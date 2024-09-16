0431c52bdc8464d4d07c1098ce641216a3cd7118
EmeBer91 <emeber987@ihmeducation.se>
2024-09-16 14:58:49 +0200

    ändringar

---
+++ PBFunction/js/app.js
@@ -0,0 +1,21 @@
+
+
+
+
+console.log("Move Forward.");
+console.log("Turn Right.");
+
+function stepForward() {
+  console.log("Move forward")
+}
+function turnRight() {
+  console.log("Turn Right")
+}
+
+function turnLeft() {
+
+    }
+
+function walkingThreeSteps() {
+  stepForward();
+}
