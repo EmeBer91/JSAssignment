f207035310fbbf8c0a642a18adcf15b6245c5d1f
EmeBer91 <emeber987@ihmeducation.se>
2024-09-16 11:21:43 +0200

    ändringar

---
+++ untitled/untitled/P8ConditionalBranching/js/app.js
@@ -0,0 +1,12 @@
+const birthYear = 2000;
+const currentYear = 2024;
+let age = currentYear - birthYear;
+
+let isChild = age <= 10;
+let isTeenager = age >= 11 && age <= 17;
+let isAdult = age >= 18;
+
+console.log(`You are a child: ${isChild}`);
+console.log(`You are a teenager: ${isTeenager}`);
+console.log(`You are an adult: ${isAdult}`);
+console.log = 
