679309e591c18730073ce3a8056d69db29e914ae
EmeBer91 <emeber987@ihmeducation.se>
2024-09-16 13:12:20 +0200

    ändringar

--- untitled/untitled/P8ConditionalBranching/js/app.js
+++ untitled/untitled/P8ConditionalBranching/js/app.js
@@ -9,4 +9,23 @@ let isAdult = age >= 18;
 console.log(`You are a child: ${isChild}`);
 console.log(`You are a teenager: ${isTeenager}`);
 console.log(`You are an adult: ${isAdult}`);
-console.log = 
+
+
+let playerTouchGround = false;
+let playerHasJetpack = true;
+
+if (playerTouchGround) {
+  console.log("Player is on the ground.");
+  console.log("Player can jump.");
+} else if (playerHasJetpack) {
+  //Runs when playerHasJetPack
+  console.log("Player can Jet Away");
+
+} else {
+  //Runs when PlayerTouchGround = false && PlayerHasJetpack
+  console.log("Player is not touching the ground.");
+  console.log("Player has no jetpack.");
+  console.log("You cannot jump right now.");
+}
+
+
